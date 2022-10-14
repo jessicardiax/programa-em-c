ee6d28dfddb3a1817e8d6cfee4f2eff7f61bd8e1
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2022-10-14 09:28:06 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float d, md, qtd;
+    printf("adicione a quantidade de maças: ");
+    scanf("%f",&d);
+    if (qtd<=md){
+    printf("0,30 a unidade");
+    }
+else
+(qtd>=d);
+printf("0,25 a unidade ");
+    return 0;
+}
